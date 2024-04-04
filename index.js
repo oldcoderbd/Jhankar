c5af8d8e29892ce9a61267135ccf5d1105e15585
oldcoderbd <oldcoderbd@gmail.com>
2024-04-05 01:08:23 +0600

    ok

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ var age = 29;
 var word = 5;
 var single = false;
 var son = true;
+var promise = "I love my beautiful country Bangladesh"
 
-console.log(typeof age);
-console.log(typeof gender);
-console.log(typeof single);
+// console.log(typeof age);
+// console.log(typeof gender);
+// console.log(typeof single);
+console.log(promise.split('desh'));
