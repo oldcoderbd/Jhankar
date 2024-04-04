33664a6085ef0f04c0c10dcd94da32eb7a051f13
oldcoderbd <oldcoderbd@gmail.com>
2024-04-05 00:58:29 +0600

    ok

---
+++ index.js
@@ -0,0 +1,10 @@
+var name = "Rohim";
+var gender = "Male";
+var age = 29;
+var word = 5;
+var single = false;
+var son = true;
+
+console.log(typeof age);
+console.log(typeof gender);
+console.log(typeof single);
