eba409863324ce2c33bbc251c9bb3b11e3bf84e5
oldcoderbd <oldcoderbd@gmail.com>
2024-04-05 21:25:31 +0600

    2nd

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-var name = "Rohim";
-var gender = "Male";
-var age = 29;
-var word = 5;
-var single = false;
-var son = true;
-var promise = "I love my beautiful country Bangladesh"
+// var name = "Rohim";
+// var gender = "Male";
+// var age = 29;
+// var word = 5;
+// var single = false;
+// var son = true;
+// var promise = "I love my beautiful country Bangladesh"
 
-// console.log(typeof age);
-// console.log(typeof gender);
-// console.log(typeof single);
-console.log(promise.split('desh'));
+// // console.log(typeof age);
+// // console.log(typeof gender);
+// // console.log(typeof single);
+// console.log(promise.split('desh'));
+
+
+
+
+
+var num1 = "26.6";
+var num2 = 19;
+num1 = parseFloat(num1);
+num1 = parseInt(num1)
+console.log(num1 + num2);
+
+var num3 = 0.1;
+var num4 = 0.2;
+var sum = num3 + num4;
+// sum = sum.toFixed(1);
+console.log(sum.toFixed(1));
+
